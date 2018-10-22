d97e3506b99f275b3b34175c0f994b67c5edacc0
Ole Jacob Brunstad <ole_jacob2@hotmail.com>
2018-10-22 16:06:39 +0200

    Bestillingsoppsett
    Funnet ut av diverse ting, hvordan vi skal velge blant forskjellige ting osv

---
+++ Prosjekt_p3/bestilling.js
@@ -0,0 +1,83 @@
+/* UFULLSTENDIG KODE, FUNKER IKKE
+function buildQuiz() {
+    const output = [];
+    // for each question...
+    myQuestions.forEach(
+        (currentQuestion, questionNumber) => {
+
+            // lagre listen med svarene
+            const answers = [];
+
+            // og for alle mulige svar 
+            for (letter in currentQuestion.answers) {
+
+                // lager vi en html radio button
+                answers.push(
+                    `<label>
+<input type="radio" name="question${questionNumber}" value="${letter}">
+${letter} :
+${currentQuestion.answers[letter]}
+</label>`
+                );
+            }
+
+            // legger spørsmålet og svarene til output
+            output.push(
+                `<div class="
+question "> ${currentQuestion.question} </div> <
+div class = "answers" > $ {
+answers.join('')
+} < /div>`
+            );
+            // finally combine our output list into one string of HTML and put it on the page
+            quizContainer.innerHTML = output.join('');
+
+        }
+
+    );
+}
+
+
+function showResults() {
+    const answerContainers = quizContainer.querySelectorAll('.answers');
+    resultsContainer.innerHTML = answerContainers;
+    const quizContainer = document.getElementById('quiz');
+    const resultsContainer = document.getElementById('results');
+    const submitButton = document.getElementById('submit');
+    const myQuestions = [
+        {
+            question: "Hva slags brød vil du ha?",
+            answers: {
+                1: "Grovt brød",
+                2: "Fint brød",
+            },
+        },
+        {
+            question: "Hva slags kjøtt vil du ha?",
+            answers: {
+                1: "Biff",
+                2: "Tartar",
+                3: "Rødbete"
+            },
+        },
+        {
+            question: "Hva slags tilbehør?",
+            answers: {
+                1: "Bacon",
+                2: "Tomat",
+                3: "Salat",
+                4: "Ost"
+            },
+
+        }
+    ];
+    // display quiz right away
+    buildQuiz();
+
+    // on submit, show results
+    submitButton.addEventListener('click', showResults);
+
+}
+
+
+*/
