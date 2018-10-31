f995e9ada0e34305df0ecdad42a175197c75a49c
paalandreeas <pamorho@gmail.com>
2018-10-31 12:15:43 +0100

    sletta unødige filer

--- forside/scripts/script.js
+++ forside/scripts/script.js
@@ -1,25 +1,18 @@
 const navbarElement = document.getElementById("navbar")
+const menuElement = document.getElementById("menu")
 const menuButtonElement = document.getElementById("menuButton")
 const resizeOn = 1;
 let show = false;
-let mobile = false;
 window.addEventListener('scroll', resizeNavbar);
 menuButtonElement.onclick = menuToggle;
-if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {
-  mobile = true;
-  menuButtonElement.style.display = "block";
-  navbarElement.id = "navbar_mobile";
-}
 
 function resizeNavbar() {
-  if (mobile === false) {
-    const scrollY = window.pageYOffset;
-    if (scrollY > resizeOn) {
-      navbarElement.classList.add("small");
-    }
-    else {
-      navbarElement.classList.remove("small");
-    }
+  const scrollY = window.pageYOffset;
+  if (scrollY > resizeOn) {
+    navbarElement.classList.add("small");
+  }
+  else {
+    navbarElement.classList.remove("small");
   }
 }
 
@@ -27,12 +20,14 @@ function menuToggle() {
   const scrollY = window.pageYOffset;
   if (show === false) {
     show = true
-    navbarElement.classList.add("show");
+    navbarElement.classList.add("toggled");
+    menuElement.classList.add("toggled");
     menuButtonElement.classList.add("toggled");
   }
   else {
     show = false
-    navbarElement.classList.remove("show");
+    navbarElement.classList.remove("toggled");
+    menuElement.classList.remove("toggled");
     menuButtonElement.classList.remove("toggled");
   }
 }
