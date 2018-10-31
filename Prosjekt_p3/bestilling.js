d49d70551acb2a6fd2866b207100e7a6b9df9f6a
Ole Jacob Brunstad <ole_jacob2@hotmail.com>
2018-10-31 12:28:46 +0100

    OJ mix n trix modal

--- Prosjekt_p3/bestilling.js
+++ Prosjekt_p3/bestilling.js
@@ -1,99 +1,58 @@
-var checkedValue = null; 
-var inputElements = document.getElementsByClassName('container');
-for(var i=0; inputElements[i]; ++i){
-      if(inputElements[i].checked){
-           checkedValue = inputElements[i].value;
-           //break;
-           
-      }
-    console.log(inputElements);
-    console.log(checkedValue + "cV")
-}
-let checkList = [];
-function addChecked() {
-    
-}
 
-/* UFULLSTENDIG KODE, FUNKER IKKE
-function buildQuiz() {
-    const output = [];
-    // for each question...
-    myQuestions.forEach(
-        (currentQuestion, questionNumber) => {
+document.getElementById("submitButton").onclick = submitOrder;
 
-            // lagre listen med svarene
-            const answers = [];
-
-            // og for alle mulige svar 
-            for (letter in currentQuestion.answers) {
-
-                // lager vi en html radio button
-                answers.push(
-                    `<label>
-<input type="radio" name="question${questionNumber}" value="${letter}">
-${letter} :
-${currentQuestion.answers[letter]}
-</label>`
-                );
-            }
-
-            // legger spørsmålet og svarene til output
-            output.push(
-                `<div class="
-question "> ${currentQuestion.question} </div> <
-div class = "answers" > $ {
-answers.join('')
-} < /div>`
-            );
-            // finally combine our output list into one string of HTML and put it on the page
-            quizContainer.innerHTML = output.join('');
-
+function submitOrder() {
+    let outputText = "Du har valgt: ";
+    let checkedElements = [];
+    inputElement = document.getElementsByClassName("input");
+    console.log(inputElement);
+    // Legger alle checkede elementene i én liste så det blir lettere å sortere når
+    //      man skal lage setninger etterpå
+    for (let i=0; i < inputElement.length; i++){
+        if (inputElement[i].checked == true){
+            checkedElements.push(inputElement[i]);
         }
+    }
 
-    );
-}
-
-
-function showResults() {
-    const answerContainers = quizContainer.querySelectorAll('.answers');
-    resultsContainer.innerHTML = answerContainers;
-    const quizContainer = document.getElementById('quiz');
-    const resultsContainer = document.getElementById('results');
-    const submitButton = document.getElementById('submit');
-    const myQuestions = [
-        {
-            question: "Hva slags brød vil du ha?",
-            answers: {
-                1: "Grovt brød",
-                2: "Fint brød",
-            },
-        },
-        {
-            question: "Hva slags kjøtt vil du ha?",
-            answers: {
-                1: "Biff",
-                2: "Tartar",
-                3: "Rødbete"
-            },
-        },
-        {
-            question: "Hva slags tilbehør?",
-            answers: {
-                1: "Bacon",
-                2: "Tomat",
-                3: "Salat",
-                4: "Ost"
-            },
-
+    console.log(checkedElements);
+    for (let i=0; i < checkedElements.length; i++){
+        // passer på at første element formateres riktig
+        if (i == 0 )  {
+            outputText =  outputText +  checkedElements[i].value;
         }
-    ];
-    // display quiz right away
-    buildQuiz();
-
-    // on submit, show results
-    submitButton.addEventListener('click', showResults);
+        else if (i == checkedElements.length - 1) {
+            outputText =  outputText + " og " + checkedElements[i].value + ".";
+        }
+        else {
+            outputText =  outputText + ", " + checkedElements[i].value;
+        }
+    }
+    console.log(outputText);
 
 }
 
+// Henter modal
+var modal = document.getElementById('myModal');
+// Henter knappen som åpner modal
+var mSubmit = document.getElementById("submitButton");
+// Henter inn elementet vi skal putte info inn i 
+var element = document.getElementById("pModal");
+// Henter span-elementet som lukker modal
+var span = document.getElementsByClassName("close")[0];
 
-*/
+// When the user clicks on the button, open the modal 
+mSubmit.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
