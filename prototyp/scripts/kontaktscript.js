e4b01bbe1e35d2095c5a9374a9af0eba196cc04d
OscarBergan <oscar.bergan@hotmail.com>
2018-11-01 11:29:07 +0100

    Endret tilbakemelding, og la til komentarer på JS

--- prototyp/scripts/kontaktscript.js
+++ prototyp/scripts/kontaktscript.js
@@ -1,12 +1,14 @@
 var submit = document.getElementById("submit");
-var melding = document.getElementById("melding")
+var melding = document.getElementById("melding");
+var email = document.getElementById("e-mail");
+var navn = document.getElementById("navn");
 submit.addEventListener('click', display);
-submit.addEventListener('click', formSubmit)
-
+submit.addEventListener('click', formSubmit);
+//Funskjon som lager en streng med navn og e-post til kunden, og legger denne til i et <p> element under submit-knappen.
 function display() {
-  melding.innerHTML = "Meldingen din er sendt";
+  melding.innerHTML = "Takk for din henvendelse, " + navn.value+ ". Du vil få svar fortløpende på: "+ email.value;
 }
-
+//Legger til en funksjon som forhindrer nettsiden i å reloade når knappen blir trykket på.
 function formSubmit(event) {
   event.preventDefault();
 }
