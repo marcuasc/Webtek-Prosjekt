948b190eb2e36e574b7bc60aabeb8601b7925fd5
paalandreeas <pamorho@gmail.com>
2018-11-01 10:31:16 +0100

    oppdaterte prototyp
    forside og kontakt funker

--- prototyp/scripts/kontaktscript.js
+++ prototyp/scripts/kontaktscript.js
@@ -1,16 +1,12 @@
 var submit = document.getElementById("submit");
 var melding = document.getElementById("melding")
-
-
-
 submit.addEventListener('click', display);
 submit.addEventListener('click', formSubmit)
 
-function display(){
-    melding.innerHTML = "Meldingen din er sendt";
-
+function display() {
+  melding.innerHTML = "Meldingen din er sendt";
 }
 
-function formSubmit(event){
+function formSubmit(event) {
   event.preventDefault();
 }
