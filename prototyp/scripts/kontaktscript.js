e2d0d8937cb2cde48704658a04a0e29ec67b5e03
OscarBergan <oscar.bergan@hotmail.com>
2018-11-06 14:13:17 +0100

    La til kommentar

--- prototyp/scripts/kontaktscript.js
+++ prototyp/scripts/kontaktscript.js
@@ -3,15 +3,17 @@ let modal = document.getElementById("myModal");
 let divModalElement = document.getElementById("divModal");
 let closeElement = document.getElementById("close");
 submitElement.addEventListener('click', displayModal);
+//Funksjon som fjerner modalen ved klikk på X-knapp.
 closeElement.onclick = function() {
   modal.style.display = "none";
 }
+//Funksjon som fjerner modalen ved klikk utenfor modalen.
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
-
+//Funksjon som sjekker om alle feltene i formen er utfylt, deretter legger den til brukerens input i modalen og sletter det som ble skrevet inn i formen av brukeren.
 function displayModal() {
   if (document.forms["form"]["navn"].value == "" || document.forms["form"]["email"].value == "" || document.forms["form"]["tekst"].value == "") {
     alert("Alle feltene må fylles ut.")
